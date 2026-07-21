074246c66b7f648b032d03a6803fb7f3dbedd0f8
gaelramos-ship-i <gael.ramos@laplateforme.io>
2026-07-21 16:09:48 +0200

    mise en place du crud

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,23 +1,11 @@
-const products = [
-    {
-        id: 1,
-        name: "Huhu",
-        price: 12.5,
-        stock: 2
-    },
-    {
-        id: 2,
-        name: "Hoho",
-        price: 13.5,
-        stock: 12
-    }
-]
+const Product = require("../models/productModel")
 
 // Fournir tous les produits 
 exports.getAllProducts = async (req, res) => {
     try {
-
-        res.json(products)
+        // Va chercher dans la bdd tous les produits
+        const product = await Product.find()
+        res.json(product)
     } catch (err) {
         res.status(500).json({ message: err.message })
     }
@@ -25,11 +13,70 @@ exports.getAllProducts = async (req, res) => {
 
 exports.getProductByID = async (req, res) => {
     try {
-        const result = products.find(product => product.id == req.params.id)
-        if(result == null){
+        const product = await Product.findById(req.params.id)
+        if(product == null){
             return res.status(404).json({message: "Produit non trouvé"})
         }
-        res.json(result)
+        res.json(product)
+    } catch (err) {
+        res.status(500).json({ message: err.message })
+    }
+}
+
+// Créer un produit
+exports.createProduct = async (req, res) => {
+    try {
+        const product = new Product({
+            name: req.body.name,
+            price: req.body.price,
+            stock: req.body.stock
+        })
+
+        const newProduct = await product.save()
+        res.status(201).json(newProduct)
+    } catch (err) {
+        res.status(400).json({ message: err.message })
+    }
+}
+
+// Mettre a jour un produit
+
+exports.updateProduct = async (req, res) => {
+    try {
+        // est ce que le produit existe 
+        const product = await Product.findById(req.params.id)
+        if(product == null){
+            return res.status(404).json({message: "Produit non trouvé"})
+        }
+
+        if(req.body.name != null){
+            product.name = req.body.name
+        }
+
+        if(req.body.price != null){
+            product.price = req.body.price
+        }
+
+        if(req.body.stock != null){
+            product.stock = req.body.stock
+        }
+
+        const updateProduct = await product.save()
+        res.json(updateProduct)
+    } catch (err) {
+        res.status(400).json({ message: err.message })
+    }
+}
+
+exports.deleteProduct = async (req, res) => {
+    try {
+        const product = await Product.findById(req.params.id)
+        if(product == null){
+            return res.status(404).json({message: "Produit non trouvé"})
+        }
+        await product.deleteOne()
+        res.json({message: "Le produit à été supprimé"})
+        
     } catch (err) {
         res.status(500).json({ message: err.message })
     }
--- routes/products.js
+++ routes/products.js
@@ -4,5 +4,8 @@ const productController = require('../controllers/productController')
 
 router.get('/', productController.getAllProducts)
 router.get('/:id', productController.getProductByID)
+router.post('/', productController.createProduct)
+router.put('/:id', productController.updateProduct)
+router.delete('/:id', productController.deleteProduct)
 
 module.exports = router
