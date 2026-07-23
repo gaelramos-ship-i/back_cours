7956c3089ed18de4eebd9222ce7122c8361c1cd7
gaelramos-ship-i <gael.ramos@laplateforme.io>
2026-07-23 12:13:40 +0200

    cours req sql

--- controllers/userSeqController.js
+++ controllers/userSeqController.js
@@ -1,9 +1,13 @@
+const { sequelize } = require('../config/database')
 const User = require('../models/userSeqModel')
+const { QueryTypes } = require('sequelize')
 
 //Get all users 
 exports.getUsers = async (req, res) => {
     try {
-        const users = await User.findAll()
+        const users = await sequelize.query('SELECT * FROM "Users"', {
+            type: QueryTypes.SELECT
+        })
         res.json(users)
     } catch (err) {
         res.status(500).json({ error: err.message })
